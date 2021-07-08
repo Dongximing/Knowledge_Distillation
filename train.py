0ecf93b15b7faaeab328ef6198f11e06e7079c4a
Ximing <dongxx1104@gmail.com>
2021-07-08 17:03:37 -0500

    a

--- train.py
+++ train.py
@@ -33,7 +33,7 @@ def train_fc(data_loader, device, model,optimizer, criterion,scheduler):
         print(outputs)
         print(targets)
         print(targets.size())
-        targets = torch.max(targets, 1)[1]
+        targets = torch.max(targets.squeeze(1), 1)[1]
         print(targets.size())
         print(targets)
         loss = criterion(outputs, targets)
