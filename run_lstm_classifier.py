9770f855e2d466de2a85daa4c02c8747d974add2
Ximing <dongxx1104@gmail.com>
2021-07-08 18:27:28 -0500

    a

--- run_lstm_classifier.py
+++ run_lstm_classifier.py
@@ -109,12 +109,12 @@ def main():
     glove_dir = '/home/dongxx/projects/def-mercer/dongxx/project/word2vec/glove.6B.100d.txt'
     word2vec_dir = '/home/dongxx/projects/def-mercer/dongxx/project/word2vec/glove.6B.word2vec.100d.txt'
     checkpoint_dir = config.MODEL_PATH
-    max_num_epochs = 10
+    max_num_epochs = 15
     num_samples = 1
     configs = {
          "hidden_dim": tune.choice([256]),
-         "lr" : tune.choice([1e-2]),
-         "batch_size": tune.choice([128])
+         "lr" : tune.choice([1e-3]),
+         "batch_size": tune.choice([64])
 
     }
     scheduler = ASHAScheduler(
