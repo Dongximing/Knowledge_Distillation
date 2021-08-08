ca65da99c57a31ef7ce175465410e1b0ea9c9661
Ximing <dongxx1104@gmail.com>
2021-08-08 15:40:32 -0500

    adding model

--- models.py
+++ models.py
@@ -26,6 +26,7 @@ class LSTMBaseline(nn.Module):
         # seq = text[idx]
 
         seq = self.dropout(self.embedding_layer(text))
+        print("emebedding")
         print(self.embedding_layer.weight[:2,:])
 
         a_packed_input = t.nn.utils.rnn.pack_padded_sequence(input=seq, lengths=text_length.to('cpu'), batch_first=True)
