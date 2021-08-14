f53946b70c4fb0a62eb7eb88a01871c1648da405
Dongximing <dongx34@myumanitoba.ca>
2021-08-13 21:36:15 -0500

    adding model

--- run_lstm_baseline_classifier.py
+++ run_lstm_baseline_classifier.py
@@ -205,7 +205,7 @@ def main():
     parser.add_argument('--lr', type=float, default=1e-3)
 
     parser.add_argument('--weight_decay', type=float, default=0.5)
-    parser.add_argument('--scheduler_step_sz', type=int, default=5)
+    parser.add_argument('--scheduler_step_sz', type=int, default=6)
     parser.add_argument('--lr_gamma', type=float, default=0.1)
     parser.add_argument('--number_class', type=int, default=2)
 
