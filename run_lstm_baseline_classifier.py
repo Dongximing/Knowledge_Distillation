67f4d926295a458d31193eeb447f0eefbb958329
Dongximing <dongx34@myumanitoba.ca>
2021-08-15 15:55:30 -0500

    adding model

--- run_lstm_baseline_classifier.py
+++ run_lstm_baseline_classifier.py
@@ -228,7 +228,7 @@ def main():
     counter1 = Counter((glove.stoi).values())
 
     counter1.update(counter2)
-    # print(counter1)
+    print(counter1)
 
     vocab = Vocab(counter1)
     vocab_size=vocab.__len__()
