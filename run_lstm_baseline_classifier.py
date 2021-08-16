12138aefa7d2aede5a12b2a58f5545dbbb067c84
Dongximing <dongx34@myumanitoba.ca>
2021-08-15 19:52:55 -0500

    adding model

--- run_lstm_baseline_classifier.py
+++ run_lstm_baseline_classifier.py
@@ -53,8 +53,8 @@ def prepare_dateset(train_data_path, validation_data_path,test_data_path,vocab):
     logging.info("Start loading training data")
     training = pd.read_csv(train_data_path)
 
-    training_review = training.Review[:2]
-    training_sentiment = training.Sentiment[:2]
+    training_review = training.Review
+    training_sentiment = training.Sentiment
 
     for text,label in zip(training_review,training_sentiment):
         training_texts.append(text)
@@ -67,8 +67,8 @@ def prepare_dateset(train_data_path, validation_data_path,test_data_path,vocab):
     logging.info("Start loading validation data")
 
     validation = pd.read_csv(validation_data_path)
-    validation_review = validation.Review[:2]
-    validation_sentiment = validation.Sentiment[:2]
+    validation_review = validation.Review
+    validation_sentiment = validation.Sentiment
 
 
     for text,label in zip(validation_review,validation_sentiment):
@@ -82,8 +82,8 @@ def prepare_dateset(train_data_path, validation_data_path,test_data_path,vocab):
     logging.info("Start loading testing data")
 
     testing = pd.read_csv(test_data_path)
-    testing_review = testing.Review[:2]
-    testing_sentiment = testing.Sentiment[:2]
+    testing_review = testing.Review
+    testing_sentiment = testing.Sentiment
     for text, label in zip(testing_review, testing_sentiment):
         testing_texts.append(text)
         testing_labels.append(label)
@@ -208,7 +208,7 @@ def main():
     parser.add_argument('--dropout', type=float, default=0.25)
     parser.add_argument('--embedding_dim', type=int, default=100)
     parser.add_argument('--num_epochs', type=int, default=16)
-    parser.add_argument('--batch_sz', type=int, default=2)
+    parser.add_argument('--batch_sz', type=int, default=32)
     parser.add_argument('--lr', type=float, default=1e-3)
 
     parser.add_argument('--weight_decay', type=float, default=0.5)
@@ -223,7 +223,7 @@ def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     # dataset
     glove = torchtext.vocab.GloVe(name='6B', dim=100,)
-    print(glove.get_vecs_by_tokens(['picture']))
+    # print(glove.get_vecs_by_tokens(['picture']))
     counter2 = Counter({'<unk>': 400000, '<pad>': 400001,'the':1})
     counter1 =  copy.deepcopy(glove.stoi)
 
@@ -233,7 +233,7 @@ def main():
     vocab = Vocab(counter1)
     vocab_size=vocab.__len__()
     print("vocab_size:",vocab_size)
-    print(vocab.stoi)
+    # print(vocab.stoi)
     #
     # print(vocab.itos[2])
     # train_dataset, validation_dataset, test_dataset, vocab, vocab_size = prepare_dateset(args.train_path,args.validation_path)
@@ -267,7 +267,7 @@ def main():
 
     best_loss = float('inf')
     print("training")
-    for epoch in range(2):
+    for epoch in range(16):
         start_time = time.time()
         # print("training emebedding")
 
