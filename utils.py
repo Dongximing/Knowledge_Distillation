a3e4fedf6d00d9798ede6ce2669e1a898b39f451
Dongximing <dongx34@myumanitoba.ca>
2021-08-18 15:02:51 -0500

    adding model

--- utils.py
+++ utils.py
@@ -73,9 +73,9 @@ def _create_data_from_iterator(vocab,tokenizer, iterator, include_unk, is_test=F
 
                     token_ids = list(filter(lambda x: x is not Vocab.UNK, [vocab[token]
                                                                            for token in text]))
-                    print(token_ids)
+                    # print(token_ids)
                     tokens = torch.tensor(token_ids)
-                    print(tokens)
+                    # print(tokens)
                     encoding = tokenizer.encode_plus(
                         bert_text,
 
