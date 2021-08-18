b9226977e749ed8dfa70d7010e41ee7391205ecf
Dongximing <dongx34@myumanitoba.ca>
2021-08-18 14:53:32 -0500

    adding model

--- utils.py
+++ utils.py
@@ -69,8 +69,7 @@ def _create_data_from_iterator(vocab,tokenizer, iterator, include_unk, is_test=F
                     bert_ids = encoding['input_ids']
                     attention_mask = encoding['attention_mask']
                 else:
-                    print(list(text))
-                    print(bert_text)
+             
 
                     token_ids = list(filter(lambda x: x is not Vocab.UNK, [vocab[token]
                                                                            for token in text]))
