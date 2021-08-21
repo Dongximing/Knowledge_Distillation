609dbecdf8b0a44a3e95ff0c716d5870c4e45352
Dongximing <dongx34@myumanitoba.ca>
2021-08-20 20:08:15 -0500

    # step 10 bs =16 ep =25

--- run_kd_lstm_classifier.py
+++ run_kd_lstm_classifier.py
@@ -228,7 +228,7 @@ def main():
     parser.add_argument('--dropout', type=float, default=0.25)
     parser.add_argument('--embedding_dim', type=int, default=100)
     parser.add_argument('--num_epochs', type=int, default=10)
-    parser.add_argument('--batch_sz', type=int, default=32)
+    parser.add_argument('--batch_sz', type=int, default=16)
     parser.add_argument('--lr', type=float, default=1e-3)
 
     parser.add_argument('--weight_decay', type=float, default=0.5)
@@ -263,7 +263,7 @@ def main():
     LSTM_model.to(device)
     #opt scheduler criterion
     optimizer = torch.optim.Adam(LSTM_model.parameters(), lr=args.lr)
-    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, gamma=args.lr_gamma, step_size=5)
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, gamma=args.lr_gamma, step_size=10)
     criterion = nn.CrossEntropyLoss()
     kd_critertion = nn.MSELoss()
     kd_critertion = kd_critertion.to(device)
@@ -298,7 +298,7 @@ def main():
 
     best_loss = float('inf')
     print("training")
-    for epoch in range(20):
+    for epoch in range(25):
         start_time = time.time()
 
 
