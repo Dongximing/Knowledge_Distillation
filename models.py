11e5f544b73226a051dabb73c594bf1b0d5f89a3
Dongximing <dongx34@myumanitoba.ca>
2021-08-22 12:39:59 -0500

    # step 7 bs =16 ep =25

--- models.py
+++ models.py
@@ -109,7 +109,7 @@ class BERTGRUSentiment(nn.Module):
             embedded = self.dropout(self.bert(ids, attention_mask=mask)[0])
 
         # embedded = [batch size, sent len, emb dim]
-        output,hidden = self.rnn(embedded)
+        output,hidden = self.LSTM(embedded)
         #_, hidden = self.rnn(embedded)
         # print(hidden.shape)
 
