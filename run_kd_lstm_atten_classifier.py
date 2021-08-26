ec3daa53ced7eb151c1728ba5b9667ec3ee7189f
Dongximing <dongx34@myumanitoba.ca>
2021-08-26 14:41:11 -0500

    add attention model

--- run_kd_lstm_atten_classifier.py
+++ run_kd_lstm_atten_classifier.py
@@ -57,8 +57,8 @@ def prepare_dateset(train_data_path, validation_data_path,test_data_path,vocab):
     logging.info("Start loading training data")
     training = pd.read_csv(train_data_path)
 
-    training_review = training.Review[:2]
-    training_sentiment = training.Sentiment[:2]
+    training_review = training.Review
+    training_sentiment = training.Sentiment
 
     for text,label in zip(training_review,training_sentiment):
         training_texts.append(text)
@@ -71,8 +71,8 @@ def prepare_dateset(train_data_path, validation_data_path,test_data_path,vocab):
     logging.info("Start loading validation data")
 
     validation = pd.read_csv(validation_data_path)
-    validation_review = validation.Review[:2]
-    validation_sentiment = validation.Sentiment[:2]
+    validation_review = validation.Review
+    validation_sentiment = validation.Sentiment
 
 
     for text,label in zip(validation_review,validation_sentiment):
@@ -86,8 +86,8 @@ def prepare_dateset(train_data_path, validation_data_path,test_data_path,vocab):
     logging.info("Start loading testing data")
 
     testing = pd.read_csv(test_data_path)
-    testing_review = testing.Review[:2]
-    testing_sentiment = testing.Sentiment[:2]
+    testing_review = testing.Review
+    testing_sentiment = testing.Sentiment
     for text, label in zip(testing_review, testing_sentiment):
         testing_texts.append(text)
         testing_labels.append(label)
