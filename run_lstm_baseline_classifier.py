f66de81e9d7047741ae0527b7cb94ba3dfa883ff
Dongximing <dongx34@myumanitoba.ca>
2021-10-27 12:38:00 +0800

    bs = 64sdadasd

--- run_lstm_baseline_classifier.py
+++ run_lstm_baseline_classifier.py
@@ -4,7 +4,7 @@ import numpy as np
 from torch.utils.data import DataLoader
 from torchtext.vocab import GloVe,Vocab
 from tqdm import tqdm
-from utils import IMDB_indexing, pad_sequence
+from utils import IMDB_indexing, pad_sequence,pad_sequencing
 from models import CNN_Baseline,LSTMBaseline
 import torchtext.vocab
 import csv
