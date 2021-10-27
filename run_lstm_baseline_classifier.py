366f69f9a06c84798934170faa6eff190d975f3a
Dongximing <dongx34@myumanitoba.ca>
2021-10-27 14:51:47 +0800

    download glove d

--- run_lstm_baseline_classifier.py
+++ run_lstm_baseline_classifier.py
@@ -204,9 +204,16 @@ def validate(validation_dataset, model, criterion, device):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--train_path',type=str,default='/home/dongxx/projects/def-mercer/dongxx/project/data/train.csv')
-    parser.add_argument('--validation_path',type= str,default='/home/dongxx/projects/def-mercer/dongxx/project/data/valid.csv')
-    parser.add_argument('--test_path',type= str,default='/home/dongxx/projects/def-mercer/dongxx/project/data/test.csv')
+    parser.add_argument('--train_path', type=str,
+                        default='/home/dongxx/projects/def-mercer/dongxx/IMDB_data/train.csv')
+    parser.add_argument('--validation_path', type=str,
+                        default='/home/dongxx/projects/def-mercer/dongxx/IMDB_data/valid.csv')
+    parser.add_argument('--test_path', type=str,
+                        default='/home/dongxx/projects/def-mercer/dongxx/IMDB_data/test.csv')
+
+    # parser.add_argument('--train_path',type=str,default='/home/dongxx/projects/def-mercer/dongxx/project/data/train.csv')
+    # parser.add_argument('--validation_path',type= str,default='/home/dongxx/projects/def-mercer/dongxx/project/data/valid.csv')
+    # parser.add_argument('--test_path',type= str,default='/home/dongxx/projects/def-mercer/dongxx/project/data/test.csv')
 
     parser.add_argument('--dropout', type=float, default=0.25)
     parser.add_argument('--embedding_dim', type=int, default=100)
