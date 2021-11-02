8525c1ca844b1adde6ea1b66529cfb4a8f841f70
Dongximing <dongx34@myumanitoba.ca>
2021-11-03 04:37:59 +0800

    add kd beluga path

--- run_kd_lstm_classifier.py
+++ run_kd_lstm_classifier.py
@@ -233,7 +233,7 @@ def main():
 
     parser.add_argument('--weight_decay', type=float, default=0.5)
     parser.add_argument('--scheduler_step_sz', type=int, default=6)
-    parser.add_argument('--lr_gamma', type=float, default=0.1)
+    parser.add_argument('--lr_gamma', type=float, default=0.5)
     parser.add_argument('--number_class', type=int, default=2)
 
     args = parser.parse_args()
@@ -283,7 +283,7 @@ def main():
     LSTM_model.to(device)
     #opt scheduler criterion
     optimizer = torch.optim.Adam(LSTM_model.parameters(), lr=args.lr)
-    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, gamma=args.lr_gamma, step_size=12)
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, gamma=args.lr_gamma, step_size=8)
     criterion = nn.CrossEntropyLoss()
     kd_critertion = nn.MSELoss()
     kd_critertion = kd_critertion.to(device)
