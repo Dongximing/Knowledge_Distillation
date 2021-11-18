813c235f0bac2a2e80d97a0d595b7e1d51c08c89
Dongximing <dongx34@myumanitoba.ca>
2021-11-18 11:35:36 +0800

    add atten kd function no drop out in hidden

--- run_bert_ft_classifier.py
+++ run_bert_ft_classifier.py
@@ -321,14 +321,14 @@ def prepare_dateset(train_data_path, validation_data_path, test_data_path):
 
     print('prepare training and test sets')
     logging.info('Prepare training and test sets')
-
+    labellist = list(testing.Sentiment)
     tokenizers = BertTokenizer.from_pretrained('/home/dongxx/projects/def-mercer/dongxx/bert-base-uncased', do_lower_case=True)
 
     train_dataset, validation_dataset, testing_dataset = bert_IMDB(training_texts, training_labels, validation_texts,
                                                                    validation_labels, testing_texts, testing_labels,
                                                                    tokenizer=tokenizers, max_len=512)
 
-    return train_dataset, validation_dataset, testing_dataset
+    return train_dataset, validation_dataset, testing_dataset,labellist
 
 
 def categorical_accuracy(preds, y):
