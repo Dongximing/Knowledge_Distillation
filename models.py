f6f49b70faa2b35a8e71b23a7aef52ce164196a0
Dongximing <dongx34@myumanitoba.ca>
2021-11-25 14:00:22 +0800

    add atten kd function no drop out in hidden

--- models.py
+++ models.py
@@ -198,10 +198,10 @@ class LSTM_atten(nn.Module):
         M = self.tanh(finial_state)  # (batch_size, word_pad_len, rnn_size)
 
         # eq.10: α = softmax(w^T M)
-        alpha = self.w(finial_state).squeeze(2)  # (batch_size, word_pad_len)
+        alpha = self.w(M).squeeze(2)  # (batch_size, word_pad_len)
         alpha = self.softmax(alpha)  # (batch_size, word_pad_len)
 
-        r = H * alpha.unsqueeze(2)  # (batch_size, word_pad_len, rnn_size)
+        r = finial_state * alpha.unsqueeze(2)  # (batch_size, word_pad_len, rnn_size)
         r = r.sum(dim = 1)  # (batch_size, rnn_size)
 
         return r, alpha
