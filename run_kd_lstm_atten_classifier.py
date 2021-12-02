ada870907daf45001a430f444617989aeef0a27c
Dongximing <dongx34@myumanitoba.ca>
2021-12-02 12:47:58 +0800

    T =2

--- run_kd_lstm_atten_classifier.py
+++ run_kd_lstm_atten_classifier.py
@@ -242,7 +242,7 @@ def main():
     parser.add_argument('--embedding_dim', type=int, default=100)
     parser.add_argument('--num_epochs', type=int, default = 20)
     parser.add_argument('--batch_sz', type=int, default=32)
-    parser.add_argument('--lr', type=float, default=1e-2)
+    parser.add_argument('--lr', type=float, default=1e-3)
 
     parser.add_argument('--weight_decay', type=float, default=0.5)
     parser.add_argument('--scheduler_step_sz', type=int, default=6)
