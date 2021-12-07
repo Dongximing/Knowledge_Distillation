49ece052f65eb395d7532785ef00932f00ee14cf
Dongximing <dongx34@myumanitoba.ca>
2021-12-07 12:00:02 +0800

    add bert ft path as teacher model

--- test.py
+++ test.py
@@ -331,7 +331,7 @@ def main():
 
     print("testing")
     LSTM_atten_model.load_state_dict(
-        torch.load('/home/dongxx/projects/def-mercer/dongxx/project/LSTM-baseline/kd_atten_test.pt'))
+        torch.load('/home/dongxx/projects/def-mercer/dongxx/project/LSTM-baseline/kd_atten.pt'))
     test_loss, test_acc = validate(testing, LSTM_atten_model, criterion, device)
 
     print(f'Test Loss: {test_loss:.3f} | Test Acc: {test_acc * 100:.2f}%')
