03fd4e5f9332824f98fcaaeaf384a11d7ad3c815
Dongximing <dongx34@myumanitoba.ca>
2022-05-23 12:50:25 +0800

    eda

--- eda.py
+++ eda.py
@@ -2,7 +2,7 @@ import pandas as pd
 import csv
 training_da = pd.read_csv('/home/dongxx/projects/def-parimala/dongxx/data/datrain.csv')
 # training_da.columns = ['Lines','ID','Review','Review clean','Rating','Sentiment','Set']
-training_da.drop(columns=['ID','Review clean','Rating','Set'],inplace=True, axis=1)
+training_da.drop(columns=['','ID','Review clean','Rating','Set'],inplace=True, axis=1)
 training_da.to_csv('/home/dongxx/projects/def-parimala/dongxx/data/datrain2.csv',encoding='utf-8')
 # with open('/home/dongxx/projects/def-parimala/dongxx/data/datrain.csv', 'r') as infile, open('/home/dongxx/projects/def-parimala/dongxx/data/reordered.csv', 'a') as outfile:
 #     # output dict needs a list for new column ordering
