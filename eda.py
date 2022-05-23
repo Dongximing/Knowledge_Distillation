6cb7662fa349dd05aff83b42e6fc9ff914bd038e
Dongximing <dongx34@myumanitoba.ca>
2022-05-23 12:22:36 +0800

    add kd_attten v2 drop out=0.5  bert lr =15

---
+++ eda.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import csv
+training_da = pd.read_csv('/home/dongxx/projects/def-parimala/dongxx/data/datrain.csv')
+training_da.columns= ['Lines','ID','Review','Review_clean','Rating','Sentiment','Set']
+training_da.drop(columns=['Lines','ID','Review_clean','Rating','Set'])
+with open('datrain.csv', 'r') as infile, open('reordered.csv', 'a') as outfile:
+    # output dict needs a list for new column ordering
+    fieldnames = ['Sentiment','Review']
+    writer = csv.DictWriter(outfile, fieldnames=fieldnames)
+    # reorder the header first
+    writer.writeheader()
+    for row in csv.DictReader(infile):
+        # writes the reordered rows to the new file
+        writer.writerow(row)
