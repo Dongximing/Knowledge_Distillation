640e7e0fe8c4d8ded4c42b3b904c64a62486dffe
Dongximing <dongx34@myumanitoba.ca>
2022-05-23 19:24:11 +0800

    A = 0.7 drop 0.25

--- eda.py
+++ eda.py
@@ -1,27 +1,20 @@
 import pandas as pd
 import csv
-# training_da = pd.read_csv('/home/dongxx/projects/def-parimala/dongxx/data/datrain.csv', index_col=0)
-# training_da.columns = ['ID','Review','Review clean','Rating','Sentiment','Set']
-# training_da.drop(columns=['ID','Review clean','Rating','Set'],inplace=True, axis=1)
-# training_da.to_csv('/home/dongxx/projects/def-parimala/dongxx/data/datrain2.csv',encoding='utf-8',index=False)
-# with open('/home/dongxx/projects/def-parimala/dongxx/data/datrain2.csv', 'r') as infile, open('/home/dongxx/projects/def-parimala/dongxx/data/reordered.csv', 'a') as outfile:
-#     # output dict needs a list for new column ordering
-#     fieldnames = ['Sentiment','Review']
-#     writer = csv.DictWriter(outfile, fieldnames=fieldnames)
-#     # reorder the header first
-#     writer.writeheader()
-#     for row in csv.DictReader(infile):
-#         # writes the reordered rows to the new file
-#         writer.writerow(row)
-# csv_file = raw_input('Enter the name of your input file: ')
-# txt_file = raw_input('Enter the name of your output file: ')
-# with open('eda.txt', "w") as my_output_file:
-#     with open('/home/dongxx/projects/def-parimala/dongxx/data/reordered.csv', "r") as my_input_file:
-#         [ my_output_file.write("\t".join(row)+'\n') for row in csv.reader(my_input_file)]
-#     my_output_file.close()
-print("123")
-print("\t123")
-print("\n123\n456\n789")
-print("\t123\t456\t789")
-print("自然数：\n\t123\n\t456\n\t789")
-print("\t\n123\t\n456\t\n789")
+training_da = pd.read_csv('/home/dongxx/projects/def-parimala/dongxx/data/datrain.csv', index_col=0)
+training_da.columns = ['ID','Review','Review clean','Rating','Sentiment','Set']
+training_da.drop(columns=['ID','Review clean','Rating','Set'],inplace=True, axis=1)
+training_da.to_csv('/home/dongxx/projects/def-parimala/dongxx/data/datrain2.csv',encoding='utf-8',index=False)
+with open('/home/dongxx/projects/def-parimala/dongxx/data/datrain2.csv', 'r') as infile, open('/home/dongxx/projects/def-parimala/dongxx/data/reordered.csv', 'a') as outfile:
+    # output dict needs a list for new column ordering
+    fieldnames = ['Sentiment','Review']
+    writer = csv.DictWriter(outfile, fieldnames=fieldnames)
+    # reorder the header first
+    writer.writeheader()
+    for row in csv.DictReader(infile):
+        # writes the reordered rows to the new file
+        writer.writerow(row)
+
+with open(''/home/dongxx/projects/def-parimala/dongxx/eda.txt', "w") as my_output_file:
+    with open('/home/dongxx/projects/def-parimala/dongxx/data/reordered.csv', "r") as my_input_file:
+        [ my_output_file.write("\t".join(row)+'\n') for row in csv.reader(my_input_file)]
+    my_output_file.close()
