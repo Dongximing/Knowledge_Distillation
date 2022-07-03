f4ae1a55f49f4bafaec7dbe7850ba363a2394752
Dongximing <dongx34@myumanitoba.ca>
2022-07-03 17:48:45 +0800

    A = 0.7 drop 0.3 lr =15

--- new_withlogit.py
+++ new_withlogit.py
@@ -292,11 +292,11 @@ def main():
 
         if valid_loss < best_loss:
             best_loss = valid_loss
-            torch.save(LSTM_atten_model.state_dict(), '/home/dongxx/projects/def-parimala/dongxx/Model_parameter/kd_atten_new1.pt')
+            torch.save(LSTM_atten_model.state_dict(), config.MODEL_Base_PATH_fk_v2_withl)
     print("training done")
 
     print("testing")
-    LSTM_atten_model.load_state_dict(torch.load('/home/dongxx/projects/def-parimala/dongxx/Model_parameter/kd_atten_new1.pt'))
+    LSTM_atten_model.load_state_dict(torch.load(config.MODEL_Base_PATH_fk_v2_withl))
     test_loss, test_acc,flat_list = validate(testing,LSTM_atten_model,criterion,device)
 
     print(f'Test Loss: {test_loss:.3f} | Test Acc: {test_acc * 100:.2f}%')
