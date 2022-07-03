aaade106a16ef42fb3b0d8d22e1d772c456af3b1
Dongximing <dongx34@myumanitoba.ca>
2022-07-03 19:40:49 +0800

    A = 0.7 drop 0.3 lr =15

--- new_withlogit.py
+++ new_withlogit.py
@@ -149,6 +149,7 @@ def train_kd_fc(data_loader, device, model,optimizer, criterion,criterion_kd,sch
         text_length = torch.Tensor(text_length)
         label = torch.tensor(label, dtype=torch.long)
         ids = text.to(device, dtype=torch.long)
+        print(logit)
 
         logit = logit.to(device,dtype=torch.float)
 
