dc8a261906448e30392f65b5bc81afbfa89d0d81
Dongximing <dongx34@myumanitoba.ca>
2022-07-03 17:42:46 +0800

    A = 0.7 drop 0.3 lr =15

--- new_withlogit.py
+++ new_withlogit.py
@@ -183,7 +183,7 @@ def validate(validation_dataset, model, criterion, device):
     epoch_acc = 0
     total_pred = []
     for i,data in enumerate(validation_dataset):
-        text, text_length, label, _, _,mask,_ = data
+        text, text_length, label, _mask= data
         text_length = torch.Tensor(text_length)
         label = torch.tensor(label, dtype=torch.long)
         text = text.to(device, dtype=torch.long)
